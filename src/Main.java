f251fbaa7c76337bb21159d0488cdc505d2d33db
Yutta <111496203+Yutta-u@users.noreply.github.com>
2022-08-18 17:26:48 +0500

    Initial commit 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+
+        if ((a < 2) || (a > 3)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+        System.out.println(" Good bye!");
+    }
+}
